8d5a2586f8d83bb5614d199abd4671eb9756fb76
dimasma0305 <dimasmaulana0305@gmail.com>
2021-11-22 23:01:55 +0800

    menambah c++

---
+++ Convertermatauang.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void bio(){
+	cout << "=====================================================" << endl;
+	cout << "nama : Dimas Maulana" << endl;
+	cout << "no   : 04" << endl;
+	cout << "kls  : XII TKJ 2" << endl;
+	cout << "=====================================================" << endl;
+}
+
+int main(){
+	int amount;
+	float result;
+	int currency;
+	string currency_name;
+	cout << "Kamu bisa konfersi mata uang rupiah ke dolar atau yen" << endl;
+	cout << "=====================================================" << endl;
+	cout << "0. [bio data]" << endl;
+	cout << "1. Rupiah ke Dolar" << endl;
+	cout << "2. Rupiah ke Yen" << endl;
+	cout << "=====================================================" << endl;
+	cout << "Masukkan opesi: ";
+	cin >> currency;
+	if (currency = 0)
+	{
+		bio();
+	}
+	else
+	{
+		cout << "Masukkan jumlah rupiah: ";
+		cin >> amount;
+		float float_amount = (float)amount;
+		switch (currency)
+		{
+		case 1:
+			currency_name = "dolar";
+			result = float_amount * 0.000070;
+			break;
+		case 2:
+			currency_name = "yen";
+			result = float_amount * 0.0080;
+			break;
+		}
+		cout << amount << " rupiah adalah " << result << " " << currency << endl;
+}
+	}
